b500d9af8516489cd8901181a53f280016ddf3f9
rachitgoyal14 <goyalrachit1410@gmail.com>
2026-07-16 13:13:52 +0530

    sum of even numbers in fibonacci

--- c_assignments/fibonacci/sumOfEvenNumbers.c
+++ c_assignments/fibonacci/sumOfEvenNumbers.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int sumOfEvenTermsInFibonacci(int n) {
+// BRUTE FORCE APPROACH
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+int bruteForceSumEvenFibonacci(int n) {
     int a = 0, b = 1, next;
-    int sumOfEvenTerms = 0;
+    int sum = 0;
 
-    for (int i = 0; i < n; i++) {
+    while (a <= n) {
         if (a % 2 == 0) {
-            sumOfEvenTerms += a;
+            sum += a;
         }
 
         next = a + b;
@@ -14,18 +17,52 @@ int sumOfEvenTermsInFibonacci(int n) {
         b = next;
     }
 
-    return sumOfEvenTerms;
+    return sum;
+}
+
+// OPTIMIZED APPROACH
+// OBSERVATION => every 3rd fibonacci number is even
+
+// Even Fibonacci recurrence:
+// (next even number) = 4 * (last even number) + (second last even number)
+// E(n) = 4 * E(n - 1) + E(n - 2)
+
+// Time Complexity: O(log(N))
+// Space Complexity: O(1)
+
+
+int optimizedSumEvenFibonacci(int n) {
+    int e1 = 0;
+    int e2 = 2;
+    int nextEven;
+    int sum = 0;
+
+    while (e1 <= n) {
+        sum += e1; // add it to the sum
+
+        nextEven = 4 * e2 + e1; // use the recurrence relation
+        
+        // move the variables to the next position
+        e1 = e2; 
+        e2 = nextEven;
+    }
+
+    return sum;
 }
 
 int main() {
     int n;
 
-    printf("Enter the number of terms: ");
+    printf("Enter the upper limit: ");
     scanf("%d", &n);
 
-    int sumOfEvenTerms = sumOfEvenTermsInFibonacci(n);
+    printf("\nBrute Force Approach\n");
+    printf("Sum of even Fibonacci numbers = %d\n",
+           bruteForceSumEvenFibonacci(n));
 
-    printf("Sum of even Fibonacci terms = %d\n", sumOfEvenTerms);
+    printf("\nOptimized Approach\n");
+    printf("Sum of even Fibonacci numbers = %d\n",
+           optimizedSumEvenFibonacci(n));
 
     return 0;
 }
