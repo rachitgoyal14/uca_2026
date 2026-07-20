185a5d92feb1d2c171491bb322f576f125fd9125
rachitgoyal14 <goyalrachit1410@gmail.com>
2026-07-20 20:51:20 +0530

    fix: code refactor to questions/ for weekly questions

--- questions/consecutiveIntegers/solution.java
+++ questions/consecutiveIntegers/solution.java
@@ -1,4 +1,4 @@
-package java_assignments.consecutiveIntegers;
+package questions.consecutiveIntegers;
 
 import java.util.*;
 
--- questions/robotMessageTracker/RobotMessageTracker.java
+++ questions/robotMessageTracker/RobotMessageTracker.java
@@ -1,3 +1,5 @@
+package questions.robotMessageTracker;
+
 import java.util.*;
 
 
