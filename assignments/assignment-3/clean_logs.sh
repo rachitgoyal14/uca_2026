70a63d082614e07ca0b131a5c2bf66a60720435f
rachitgoyal14 <goyalrachit1410@gmail.com>
2026-07-25 04:31:07 +0530

    Add sed solution for log cleanup (macOS-compatible)

--- assignments/assignment-3/clean_logs.sh
+++ assignments/assignment-3/clean_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed -i -E '
+sed -i '' -E '
 /^#/d
-s/\b[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}\b/XXXX-XXXX-XXXX-XXXX/g
-s/\b[0-9]{16}\b/XXXX-XXXX-XXXX-XXXX/g
+s/[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/XXXX-XXXX-XXXX-XXXX/g
+s/[0-9]{16}/XXXX-XXXX-XXXX-XXXX/g
 s/HTTP\/1\.0/HTTP\/1.1/g
 ' server_access.log
